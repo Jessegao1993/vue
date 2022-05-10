8971895cf2ffe463a688d9bbd4a3940588fe25ba
JesseGaoMac <gaoqijesse@gmail.com>
2022-05-10 18:05:34 +0800

    20220510

--- vueStart/37vuecli/src/App.vue
+++ vueStart/37vuecli/src/App.vue
@@ -1,23 +1,106 @@
 <template>
-    <div>
-        <Student/>
-        <Company/>
+    <div id="root">
+        <div class="todo-container">
+            <div class="todo-wrap">
+                <MyHeader :addTodo="addTodo"></MyHeader>
+                <MyList :todos="todos" :checkTodo="checkTodo" :deleteTodo="deleteTodo"></MyList>
+                <MyFooter :todos="todos" :checkAllTodo="checkAllTodo"></MyFooter>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import Company from './components/Company.vue'
-import Student from './components/Student.vue'
+import MyHeader from './components/MyHeader.vue'
+import MyList from './components/MyList.vue'
+import MyFooter from './components/MyFooter.vue'
 
     export default {
         name:'App',
         components:{
-            Student,
-            Company
+            MyHeader,
+            MyList,
+            MyFooter
         },
+         data(){
+        return{
+            todos:[
+                {id:'001',title:'吃饭',done:true},
+                {id:'002',title:'喝水',done:false},
+                {id:'003',title:'健身',done:true}
+            ]
+        }
+        },
+        methods:{
+            // 增加一个代办事项
+            addTodo(todoObj){
+                this.todos.unshift(todoObj)
+            },
+            // 勾选&取消勾选
+            checkTodo(id){
+                this.todos.forEach(todo => {
+                    if (todo.id===id) {
+                        todo.done = !todo.done
+                    }
+                });
+            },
+            // 删除
+            deleteTodo(id){
+                this.todos = this.todos.filter((todo)=>{
+                   return todo.id != id
+                })
+            },
+            // 全选&全不选
+            checkAllTodo(done){
+                this.todos.forEach((todo)=>{
+                    todo.done = done
+                })
+            }   
+        }
     }
 </script>
 
 <style>
+    /*base*/
+    body {
+    background: #fff;
+    }
 
+    .btn {
+    display: inline-block;
+    padding: 4px 12px;
+    margin-bottom: 0;
+    font-size: 14px;
+    line-height: 20px;
+    text-align: center;
+    vertical-align: middle;
+    cursor: pointer;
+    box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+    border-radius: 4px;
+    }
+
+    .btn-danger {
+    color: #fff;
+    background-color: #da4f49;
+    border: 1px solid #bd362f;
+    }
+
+    .btn-danger:hover {
+    color: #fff;
+    background-color: #bd362f;
+    }
+
+    .btn:focus {
+    outline: none;
+    }
+
+    .todo-container {
+    width: 600px;
+    margin: 0 auto;
+    }
+    .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    }
 </style>
