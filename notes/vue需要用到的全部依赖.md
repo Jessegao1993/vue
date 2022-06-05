57ce943a6579c12b458a5a40dd65fad339c48cc0
Jesse Gao <jessegao@192.168.0.103>
2022-06-05 21:54:15 +0800

    20220605

---
+++ notes/vue需要用到的全部依赖.md
@@ -0,0 +1,15 @@
+#### 1. vue脚手架
+- 安装命令：`npm install -g @vue/cli`
+- 注意：全局安装（仅第一次执行）
+#### 2. pubSubJS（消息订阅与发布的依赖）
+- 安装命令：`npm install -S pubsub-js`
+#### 3. axios(ajax跨域)
+- 安装命令：`npm install axios`
+#### 4. vue-router
+- 安装命令： `npm install vue-router@3`
+- 作用：实现SPA应用的插件库(安装匹配vue2的对应版本)
+
+
+
+#### 10. 恢复依赖
+- 安装命令：`sudo rm -rf node_modules package-lock.json && npm install`
--- vueStart/37vuecli/src/main.js
+++ vueStart/37vuecli/src/main.js
@@ -2,23 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 // 引入插件
 //import plugins from './plugins'
-import vueResource from 'vue-resource'
-// 引入store
-import store from './store/index.js'
+// 引入路由插件
+import VueRouter from 'vue-router'
+//import vueConfig from 'vue.config'
+// 引入路由器
+import router from './router'
+
+//使用路由插件
+Vue.use(VueRouter)
 
 // 关闭vue的生产提示
 Vue.config.productionTip = false
 
-// 应用插件
-//Vue.use(plugins)
-Vue.use(vueResource)
-
 
 new Vue({
   el:'#app',
-  render: h => h(App),
-  store,
-  beforeCreate(){
-    Vue.prototype.$bus = this //注册全局事件总线
-  }
+  render: h => h(App), 
+  router:router
 })
