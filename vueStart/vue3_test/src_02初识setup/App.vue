57be916c0b08361050d1f83c33ad0a255d901f99
Jesse Gao <jessegao@192.168.0.101>
2022-06-16 07:29:13 +0800

    20220515

---
+++ vueStart/vue3_test/src_02初识setup/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>一个人的信息</h1>
+  <h2>姓名：{{name}}</h2>
+  <h2>年龄：{{age}}</h2>
+  <button @click="sayHello">说话</button>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  // 测试setup暂时不考虑响应式
+  setup(){
+    let name = 'Jesse'
+    let age = 18
+
+    function  sayHello() {
+        console.log(`Hello, my name is ${name} and my age is ${age}`)
+    }
+// 返回一个对象（常用）
+    return {
+      name,
+      age,
+      sayHello
+    }
+  }
+}
+</script>
+
+
